bba52f62e37cac61064a43bc0cfd9bb438424f33
kaellvin <kaellvin@hotmail.com>
2025-12-31 14:37:49 +0800

    Initial Commit

---
+++ simple-e-commerce-app-storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple-e-commerce-app-storyboard
+//
+//  Created by Kelvin Leong on 31/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
