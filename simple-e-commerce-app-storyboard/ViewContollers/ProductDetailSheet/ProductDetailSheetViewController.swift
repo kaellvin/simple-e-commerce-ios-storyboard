9bdced1359faeb5a6c3a65515bb0db99cfccc5f0
kaellvin <kaellvin@hotmail.com>
2026-01-13 23:09:01 +0800

    - added quantity increment/decrement buttons in ProductDetailSheetViewController

--- simple-e-commerce-app-storyboard/ViewContollers/ProductDetailSheet/ProductDetailSheetViewController.swift
+++ simple-e-commerce-app-storyboard/ViewContollers/ProductDetailSheet/ProductDetailSheetViewController.swift
@@ -13,6 +13,9 @@ class ProductDetailSheetViewController: UIViewController {
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var stockLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
     
     private var viewModel: ProductDetailSheetViewModel
     private var cancellables = Set<AnyCancellable>()
@@ -28,12 +31,24 @@ class ProductDetailSheetViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureButton(minusButton, systemName: "minus")
+        configureButton(plusButton, systemName: "plus")
+        
         bind()
-    
+
         viewModel.initData()
     }
     
-
+    private func configureButton(_ button: UIButton, systemName: String) {
+        var config = UIButton.Configuration.bordered()
+        config.cornerStyle = .capsule
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 17, weight: .regular, scale: .default)
+        config.image = UIImage(systemName: systemName, withConfiguration: imageConfig)
+        button.configuration = config
+    }
+    
+    
+    
     
     private func bind() {
         viewModel
@@ -53,23 +68,49 @@ class ProductDetailSheetViewController: UIViewController {
             .combineLatest(viewModel.$quantity)
             .sink { [weak self] activeVariant, quantity in
                 guard let self else { return }
-                        let price = Double(activeVariant.price) ?? 0.0
-                        let currentPrice = price * Double(quantity)
+                let price = Double(activeVariant.price) ?? 0.0
+                let currentPrice = price * Double(quantity)
                 
                 self.priceLabel.text = "Price: \(Int(currentPrice).formatted(.currency(code: "MYR")))"
                 self.stockLabel.text = "Stock: \(activeVariant.quantity)"
             }.store(in: &cancellables)
+        
+        viewModel
+            .$quantity
+            .map(String.init)
+            .assign(to: \.text, on: quantityLabel)
+            .store(in: &cancellables)
+        
+        viewModel
+            .$quantity
+            .map { $0 > 1 }
+            .assign(to: \.isEnabled, on: minusButton)
+            .store(in: &cancellables)
+
+        viewModel
+            .$quantity
+            .combineLatest(viewModel.$activeVariant)
+            .map { $0 < $1.quantity }
+            .assign(to: \.isEnabled, on: plusButton)
+            .store(in: &cancellables)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onMinusButtonClicked(_ sender: Any) {
+        viewModel.setQuantity(isAdding: false)
     }
-    */
-
+    
+    @IBAction func onPlusButtonClicked(_ sender: Any) {
+        viewModel.setQuantity()
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
